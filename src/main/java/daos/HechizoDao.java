132424a48faf8fc5f1be49bfdd3460264c5da346
Alexia Jauregui <105063119+alexia2212@users.noreply.github.com>
2022-11-04 11:47:53 -0500

    lista de hechizos

--- src/main/java/daos/HechizoDao.java
+++ src/main/java/daos/HechizoDao.java
@@ -1,4 +1,37 @@
 package daos;
 
+import beans.Hechizos;
+
+import java.sql.*;
+import java.util.ArrayList;
+
 public class HechizoDao {
+    public ArrayList<Hechizos> listarHechizos() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        String url = "jdbc:mysql://localhost:3306/lab8";
+        ArrayList<Hechizos> lista = new ArrayList<>();
+        String sql = "select * from hechizos";
+
+        try (Connection connection = DriverManager.getConnection(url, "root", "root");
+             Statement stmt = connection.createStatement();
+             ResultSet rs = stmt.executeQuery(sql);) {
+            while (rs.next()) {
+                Hechizos hechizos = new Hechizos();
+                hechizos.setNombreHechizo(rs.getString(1));
+                hechizos.setPotencia(rs.getInt(2));
+                hechizos.setPrecision(rs.getInt(3));
+                hechizos.setNivelAprendizaje(rs.getInt(4));
+                hechizos.setHechizoBase(rs.getInt(5));
+                lista.add(hechizos);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return lista;
+    }
 }
