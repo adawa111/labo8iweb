92cb25d7d51203eef3992e1712f2ca608ab95d41
Alexia Jauregui <105063119+alexia2212@users.noreply.github.com>
2022-11-03 20:22:21 -0500

    get and set enemigos

--- src/main/java/beans/enemigos.java
+++ src/main/java/beans/enemigos.java
@@ -1,4 +1,58 @@
 package beans;
 
 public class enemigos {
+    private String nombre;
+    private int ataque;
+    private int experiencia;
+    private String objeto;
+    private float probabilidadObjeto;
+    private String genero;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAtaque() {
+        return ataque;
+    }
+
+    public void setAtaque(int ataque) {
+        this.ataque = ataque;
+    }
+
+    public int getExperiencia() {
+        return experiencia;
+    }
+
+    public void setExperiencia(int experiencia) {
+        this.experiencia = experiencia;
+    }
+
+    public String getObjeto() {
+        return objeto;
+    }
+
+    public void setObjeto(String objeto) {
+        this.objeto = objeto;
+    }
+
+    public float getProbabilidadObjeto() {
+        return probabilidadObjeto;
+    }
+
+    public void setProbabilidadObjeto(float probabilidadObjeto) {
+        this.probabilidadObjeto = probabilidadObjeto;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
 }
