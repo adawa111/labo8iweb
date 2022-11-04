76bdcf415e35a1c48f77fa3668144f977a6e7a79
Alexia Jauregui <105063119+alexia2212@users.noreply.github.com>
2022-11-04 13:51:41 -0500

    falta añadir guardar, borrar, editar

--- src/main/java/daos/ObjetoDao.java
+++ src/main/java/daos/ObjetoDao.java
@@ -1,4 +1,38 @@
 package daos;
+
 import beans.Objetos;
+
+import java.sql.*;
+import java.util.ArrayList;
+
 public class ObjetoDao {
+    public ArrayList<Objetos> listarObjetos() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        String url = "jdbc:mysql://localhost:3306/lab8";
+        ArrayList<Objetos> lista = new ArrayList<>();
+        String sql = "select * from objetos";
+
+        try (Connection connection = DriverManager.getConnection(url, "root", "root");
+             Statement stmt = connection.createStatement();
+             ResultSet rs = stmt.executeQuery(sql);) {
+
+            while (rs.next()) {
+                Objetos objetos = new Objetos();
+                objetos.setNombreobjeto(rs.getString(1));
+                objetos.setEfecto(rs.getString(2));
+                objetos.setPeso(rs.getFloat(3));
+                objetos.setUsado(rs.getBoolean(4));
+
+                lista.add(objetos);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return lista;
+    }
 }
