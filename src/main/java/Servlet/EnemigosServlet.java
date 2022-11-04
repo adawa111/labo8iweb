3de459b867b310376610e0de0d8c50f0ebd21cff
Alexia Jauregui <105063119+alexia2212@users.noreply.github.com>
2022-11-04 17:53:16 -0500

    menu de enemigos

--- src/main/java/Servlet/EnemigosServlet.java
+++ src/main/java/Servlet/EnemigosServlet.java
@@ -1,15 +1,25 @@
 package Servlet;
 
+import beans.Enemigos;
+import daos.EnemigoDao;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.ArrayList;
 
 @WebServlet(name = "EnemigosServlet", value = "/EnemigosServlet")
 public class EnemigosServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EnemigoDao enemigoDao = new EnemigoDao();
+        ArrayList<Enemigos> lista = enemigoDao.listarEnemigos();
 
+        request.setAttribute("listaEnemigos", lista);
+
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("menuenemigos.jsp");
+        requestDispatcher.forward(request,response);
     }
 
     @Override
