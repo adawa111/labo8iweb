0f412dfd33b629b85b749f457f6e525f1eb150e5
Alexia Jauregui <105063119+alexia2212@users.noreply.github.com>
2022-11-04 11:24:15 -0500

    falta añadir queries y listas

--- src/main/java/daos/EnemigoDao.java
+++ src/main/java/daos/EnemigoDao.java
@@ -1,4 +1,77 @@
 package daos;
+
 import beans.Enemigos;
+
+import java.sql.*;
+import java.util.ArrayList;
 public class EnemigoDao {
+
+    public ArrayList<Enemigos> listarEnemigos(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }catch (ClassNotFoundException e){
+            throw new RuntimeException(e);
+        }
+
+        String url = "jdbc:mysql://localhost:3306/hr"; //modificar HR
+        ArrayList<Enemigos> lista = new ArrayList<>();
+        String sql = "select * from enemigos";
+
+        try (Connection connection = DriverManager.getConnection(url, "root", "root");
+             Statement stmt = connection.createStatement();
+             ResultSet rs = stmt.executeQuery(sql);) {
+            while (rs.next()) {
+                Enemigos enemigos = new Enemigos();
+
+                enemigos.setIdEnemigo(rs.getInt(1));
+                enemigos.setNombre(rs.getString(2));
+                enemigos.setAtaque(rs.getInt(3));
+                enemigos.setExperiencia(rs.getInt(4));
+                enemigos.setObjeto(rs.getString(5));
+                enemigos.setExperiencia(rs.getInt(6));
+                enemigos.setGenero(rs.getString(7));
+
+                lista.add(enemigos);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return lista;
+    }
+
+
+    public ArrayList<Enemigos> listarObjetosDeEnemigos() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        String url = "jdbc:mysql://localhost:3306/hr";
+        ArrayList<Enemigos> lista = new ArrayList<>();
+        String sql = "select * from enemigos"; //modificar Query
+        try (Connection connection = DriverManager.getConnection(url, "root", "root");
+             Statement stmt = connection.createStatement();
+             ResultSet rs = stmt.executeQuery(sql);) {
+
+            while (rs.next()) {
+
+                Enemigos enemigos = new Enemigos();
+
+                enemigos.setIdEnemigo(rs.getInt(1));
+                enemigos.setNombre(rs.getString(2));
+                enemigos.setAtaque(rs.getInt(3));
+                enemigos.setExperiencia(rs.getInt(4));
+                enemigos.setObjeto(rs.getString(5));
+                enemigos.setExperiencia(rs.getInt(6));
+                enemigos.setGenero(rs.getString(7));
+
+                lista.add(enemigos);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return lista;
+    }
 }
