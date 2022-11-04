c0dab786b08cf8fbef473eb3cc71fba2702b4769
Alexia Jauregui <105063119+alexia2212@users.noreply.github.com>
2022-11-04 13:43:15 -0500

    listar Heroes

--- src/main/java/daos/HeroeDao.java
+++ src/main/java/daos/HeroeDao.java
@@ -1,51 +1,46 @@
 package daos;
+
 import beans.Heroes;
-import com.mysql.cj.jdbc.Driver;
 
 import java.sql.*;
 import java.util.ArrayList;
 
 public class HeroeDao {
-    public ArrayList<Heroes> obtenerListaHeroes(){
-        ArrayList<Heroes> listaHeroes = new ArrayList<>();
-        String user = "usuario";
-        String pass = "password";
-        String url = "jbdc:mysql://localhost:3306/FINALFANTASYLAB8?serverTimezone=America/Lima";
-
-        try{
-
+    public ArrayList<Heroes> listarHeroes() {
+        try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection conn = DriverManager.getConnection(url,user,pass);
-            Statement stmt = conn.createStatement();
-            float exp = 0;
-            int level = 0;
-            ResultSet rs = stmt.executeQuery("select  * from heroes");
-            while (rs.next()){
-                Heroes heroe = new Heroes();
-                heroe.setNombre(rs.getString("nombre"));
-                heroe.setClase(rs.getString("clase"));
-                heroe.setEdad(rs.getInt("edad"));
-                heroe.setNivel(rs.getInt("nivel"));
-                level = rs.getInt("nivel");
-                if (0<level && level<=15){
-                    exp = (level*level*level)*(24+((level+1)/3))/50;
-                } else if (16<=level && level<=35) {
-                    exp = (level*level*level)*(14+level)/50;
-                } else if (36<=level && level<=100) {
-                    exp = (level*level*level)*(35+((level)/2))/50;
-                }
-                heroe.setAtaque(rs.getInt("ataque"));
-                heroe.setGenero(rs.getString("genero"));
-                heroe.setExperiencia(exp);
-                heroe.setPareja(rs.getInt("pareja"));
-
-                listaHeroes.add(heroe);
-            }
-
-        } catch (ClassNotFoundException | SQLException e){
-            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
         }
-        return listaHeroes;
+
+        String url = "jdbc:mysql://localhost:3306/lab8";
+        ArrayList<Heroes> lista = new ArrayList<>();
+        String sql = "select * from heroes";
+
+        try (Connection connection = DriverManager.getConnection(url, "root", "root");
+             Statement stmt = connection.createStatement();
+             ResultSet rs = stmt.executeQuery(sql);) {
+            while (rs.next()) {
+
+                Heroes heroes = new Heroes();
+                heroes.setNombre(rs.getString(1));
+                heroes.setEdad(rs.getInt(2));
+                heroes.setGenero(rs.getString(3));
+                heroes.setNivel(rs.getInt(4));
+                heroes.setAtaque(rs.getString(5));
+                heroes.setPareja(rs.getInt(6));
+                heroes.setExperiencia(rs.getFloat(7));
+                heroes.setClase(rs.getString(8));
+
+                lista.add(heroes);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return lista;
     }
+
+
+
 
 }
