ff7cf8b8c89672c2ceacc3368eb8807d43a38017
Alexia Jauregui <105063119+alexia2212@users.noreply.github.com>
2022-11-05 14:11:40 -0500

    bean elementos completos

---
+++ src/main/java/beans/Elementos.java
@@ -0,0 +1,22 @@
+package beans;
+
+public class Elementos {
+    private int idElementos;
+    private String nombreElemento;
+
+    public int getIdElementos() {
+        return idElementos;
+    }
+
+    public void setIdElementos(int idElementos) {
+        this.idElementos = idElementos;
+    }
+
+    public String getNombreElemento() {
+        return nombreElemento;
+    }
+
+    public void setNombreElemento(String nombreElemento) {
+        this.nombreElemento = nombreElemento;
+    }
+}
